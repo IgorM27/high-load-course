2b958cfbf2d0e16d0431d980e85e340a5bf19659
Nazar Vakulenko <nazardrakulito@gmail.com>
2025-10-31 00:28:08 +0300

    It's a kind of magic

--- src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiter.kt
+++ src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiter.kt
@@ -16,20 +16,24 @@ class LeakingBucketRateLimiter(
     bucketSize: Int,
 ) : RateLimiter {
     private val rateLimiterScope = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
-    private val queue = LinkedBlockingQueue<Int>(bucketSize)
+    private val queue = LinkedBlockingQueue<() -> Unit>(bucketSize)
 
-    override fun tick(): Boolean {
-        return queue.offer(1)
+    fun tick(task: () -> Unit): Boolean {
+        return queue.offer(task)
     }
 
     private val releaseJob = rateLimiterScope.launch {
         while (true) {
             delay(window.toMillis())
-            for (i in 0..rate) {
+            for (i in 0 until rate) {
                 queue.poll()
             }
         }
     }.invokeOnCompletion { th -> if (th != null) logger.error("Rate limiter release job completed", th) }
+
+    override fun tick(): Boolean {
+        return queue.offer {}
+    }
 
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(LeakingBucketRateLimiter::class.java)
