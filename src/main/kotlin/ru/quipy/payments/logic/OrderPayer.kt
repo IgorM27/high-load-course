9d68db27dd68cdff959b11a30177c5d7263eaa5f
Nazar Vakulenko <nazardrakulito@gmail.com>
2025-11-11 23:37:55 +0300

    feat: improve rateLimiter and retry system, change bucket size

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -42,9 +42,9 @@ class OrderPayer {
     )
 
     var limiter = LeakingBucketRateLimiter(
-        rate = 11,
+        rate = 100,
         window = Duration.ofSeconds(1),
-        bucketSize = 270
+        bucketSize = 1950
     )
 
     fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long? {
